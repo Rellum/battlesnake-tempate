b8304c71b489ec29f8df6674e4424c942cfb811c
vaughan <vaughan@luno.com>
2021-06-13 16:33:58 +0200

    battlesnake: Rename heroku dyno to web

--- heroku/main.go
+++ heroku/main.go
@@ -14,43 +14,6 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-type grid map[rules.Point]rune
-
-type Game struct {
-	ID      string `json:"id"`
-	Timeout int32  `json:"timeout"`
-}
-
-type Battlesnake struct {
-	ID     string        `json:"id"`
-	Name   string        `json:"name"`
-	Health int32         `json:"health"`
-	Body   []rules.Point `json:"body"`
-	Head   rules.Point   `json:"head"`
-	Length int32         `json:"length"`
-	Shout  string        `json:"shout"`
-}
-
-type BattlesnakeInfoResponse struct {
-	APIVersion string `json:"apiversion"`
-	Author     string `json:"author"`
-	Color      string `json:"color"`
-	Head       string `json:"head"`
-	Tail       string `json:"tail"`
-}
-
-type GameRequest struct {
-	Game  Game             `json:"game"`
-	Turn  int              `json:"turn"`
-	Board rules.BoardState `json:"board"`
-	You   Battlesnake      `json:"you"`
-}
-
-type MoveResponse struct {
-	Move  string `json:"move"`
-	Shout string `json:"shout,omitempty"`
-}
-
 //go:embed info.json
 
 var snakeInfo []byte
